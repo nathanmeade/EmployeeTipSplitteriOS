9d54516d6f0bb67740c60e2f0b5cda984389bad5
nathanmeade <nathan.r.meade@gmail.com>
2023-03-21 00:53:03 -0400

    Initial Commit

---
+++ Employee Tip Splitter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Employee Tip Splitter
+//
+//  Created by Nathan Meade on 3/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Employee Tip Splitter/Employee_Tip_SplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Employee_Tip_SplitterApp.swift
+//  Employee Tip Splitter
+//
+//  Created by Nathan Meade on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Employee_Tip_SplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
